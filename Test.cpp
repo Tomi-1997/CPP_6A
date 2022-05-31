7b5d339000a02d8faf6823a416c2dab06030a861
Tomi <74303735+Tomi-1997@users.noreply.github.com>
2022-05-31 16:21:15 +0300

    Add files via upload

---
+++ Test.cpp
@@ -0,0 +1,55 @@
+#include "doctest.h"
+#include "Team.hpp"
+#include "Game.hpp"
+#include "League.hpp"
+#include "Schedule.hpp"
+
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+#include <exception>
+#include <string>
+#include <algorithm>
+
+using namespace bball;
+using namespace std;
+
+TEST_CASE("Test Team")
+{
+    CHECK(1 == 1);
+}
+
+TEST_CASE("Test Game")
+{
+    CHECK(1 == 1);
+}
+
+TEST_CASE("Test League")
+{
+    // vector<Team*> myTeams;
+    // myTeams.push_back(new Team("Golden State Warriors", 1));
+    // myTeams.push_back(new Team("Boston Celtics", 0.4));
+    // myTeams.push_back(new Team("Brooklyn Nets", 0.5));
+    // myTeams.push_back(new Team("Denver Nuggets", 0.3));
+    // myTeams.push_back(new Team("Philadelphia 76ers", 0.4));
+    // myTeams.push_back(new Team("Minnesota Timberwolves", 0.5));
+    // myTeams.push_back(new Team("Chicago Bulls", 0.3));
+    // myTeams.push_back(new Team("Portland Trail Blazers", 0.2));
+    // myTeams.push_back(new Team("LA Clippers", 0.5));
+    // myTeams.push_back(new Team("Los Angeles Lakers", 1));
+    // myTeams.push_back(new Team("Milwaukee Bucks", 0.7));
+    // myTeams.push_back(new Team("Atlanta Hawks", 0.3));
+    // myTeams.push_back(new Team("Charlotte Hornets", 0.2));
+    // myTeams.push_back(new Team("Miami Heat", 0.6));
+    // myTeams.push_back(new Team("Phoenix Suns", 0.8));
+    // myTeams.push_back(new Team("Dallas Mavericks", 0.6));
+    // myTeams.push_back(new Team("Memphis Grizzlies", 0.5));
+    // myTeams.push_back(new Team("Utah Jazz", 0.3));
+    // myTeams.push_back(new Team("Washington Wizards", 0));
+    // myTeams.push_back(new Team("Oklahoma City Thunder", 0.1));
+}
+
+TEST_CASE("Test Schedule")
+{
+    CHECK(1 == 1);
+}
